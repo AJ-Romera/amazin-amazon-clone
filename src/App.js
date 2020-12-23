5cfe18e283da424317c49f03c0eba6554611bc56
AJ-Romera <ajromeraalfonso@gmail.com>
2020-12-23 19:04:40 +0100

    Initialize npx create-react-app and start Header Component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+    return (
+        <div className="app">
+            <h1>Look, this is the beginning of something Amazin!</h1>
+            {/* Header */}
+            {/* Home */}
+        </div>
+    );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: "AIzaSyBVDIhcemP3weVv8ce0anFzZHLVlPzrmaY",
+	authDomain: "amazin-ecommerce.firebaseapp.com",
+	projectId: "amazin-ecommerce",
+	storageBucket: "amazin-ecommerce.appspot.com",
+	messagingSenderId: "1089226848481",
+	appId: "1:1089226848481:web:022b83db6d03e1b80b9fa0",
+	measurementId: "G-WTGPSWJV5J",
+};
