0c95243576c1310d5969734964b0342659af0b53
AJ-Romera <ajromeraalfonso@gmail.com>
2020-12-25 14:03:25 +0100

    Create SubHeader Component, structure and style it

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
+
+/* Components */
 import Header from './Header';
+import SubHeader from './SubHeader';
 
 function App() {
     return (
         <div className='app'>
             <Header />
-            {/* <SubHeader /> */}
+            <SubHeader />
             {/* Home */}
         </div>
     );
---
+++ src/SubHeader.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './SubHeader.css';
+
+/* Icons */
+import MenuRoundedIcon from '@material-ui/icons/MenuRounded';
+
+function SubHeader() {
+    return (
+        <div className='subHeader'>
+            <div className='subHeader__left'>
+                <div className='subHeader__menu'>
+                    <MenuRoundedIcon className='subHeader__menuIcon' />
+                    <span className='subHeader__all'>All</span>
+                </div>
+
+                <div className='subHeader__nav'>
+                    <span className='subHeader__option'>Today's Deals</span>
+                    <span className='subHeader__option'>Customer Service</span>
+                    <span className='subHeader__option'>Gift Cards</span>
+                    <span className='subHeader__option'>Sell</span>
+                    <span className='subHeader__option'>Registry</span>
+                </div>
+            </div>
+
+            <div className='subHeader__navMsg'>
+                <span className='subHeader__msg'>
+                    Amazin response to COVID-19
+                </span>
+            </div>
+        </div>
+    );
+}
+
+export default SubHeader;
