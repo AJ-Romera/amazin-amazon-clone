f6e630aa5ae2dc9294645dc6d865dcddb0a0d245
AJ-Romera <ajromeraalfonso@gmail.com>
2020-12-26 19:12:06 +0100

    Work in Home Component (Basic structure and styling)

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import './App.css';
 /* Components */
 import Header from './Header';
 import SubHeader from './SubHeader';
+import Home from './Home';
 
 function App() {
     return (
         <div className='app'>
             <Header />
             <SubHeader />
-            {/* Home */}
+            <Home />
         </div>
     );
 }
