6f120b3a60471c7fbd61efad4e5a1c093df87be3
AJ-Romera <ajromeraalfonso@gmail.com>
2020-12-29 17:56:37 +0100

    FIX: CardUiPair CSS Causing Horizontal Scroll

--- src/CardUiPair.js
+++ src/CardUiPair.js
@@ -5,20 +5,18 @@ function CardUiPair() {
     return (
         <div className='cardUiPair'>
             <div className='cardUiPair__sectionOne'>
-                <div className='cardUiPair__containerOne'>
-                    <div className='cardUiPair__header'>
-                        <h2>Sign in for the best experience</h2>
-                    </div>
-                    <div className='cardUiPair__body'>
-                        <a href='https://www.google.es'>
-                            <button>Sign in securely</button>
-                        </a>
-                    </div>
+                <div className='cardUiPair__header'>
+                    <h2>Sign in for the best experience</h2>
+                </div>
+                <div className='cardUiPair__body'>
+                    <a href='https://www.google.es'>
+                        <button>Sign in securely</button>
+                    </a>
                 </div>
             </div>
 
             <div className='cardUiPair__sectionTwo'>
-                <div className='cardUiPair__containerTwo'>
+                <div className='cardUiPair__container'>
                     <a href='https://www.google.es'>
                         <img
                             src='https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/October/Fuji_D2_45M_en_US_1x._CB418309979_.jpg'
