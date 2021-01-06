967bfd6452766db5df789563a65c5f7f6493b6d4
AJ-Romera <ajromeraalfonso@gmail.com>
2021-01-06 16:58:32 +0100

    FIX: Slider Arrow Buttons Now Shows Up on Carousel Hover

--- src/ProductSlider.js
+++ src/ProductSlider.js
@@ -4,7 +4,7 @@ import Item from './Item';
 import './ProductSlider.css';
 
 /* const breakPoints = [
-    { width: 1, itemsToShow: 1 },
+    { width: 1, itemsToShow: 1, itemsToScroll: 1 },
     { width: 550, itemsToShow: 2 },
     { width: 768, itemsToShow: 3 },
     { width: 1200, itemsToShow: 4 },
