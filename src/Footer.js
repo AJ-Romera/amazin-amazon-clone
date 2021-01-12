c94b734ba9b0177563ba38a9176b59a05fcff406
AJ-Romera <ajromeraalfonso@gmail.com>
2021-01-12 17:46:30 +0100

    CHNG: Enhance FooterColumns component, render it and use props

--- src/Footer.js
+++ src/Footer.js
@@ -13,10 +13,82 @@ function Footer() {
                 </a>
 
                 <div className='footer__row'>
-                    <FooterColumns />
-                    <FooterColumns />
-                    <FooterColumns />
-                    <FooterColumns />
+                    <FooterColumns
+                        footerTitle='Get to Know Us'
+                        aHref1='#'
+                        aHref2='#'
+                        aHref3='#'
+                        aHref4='#'
+                        aHref5='#'
+                        aHref6='#'
+                        aHref7=''
+                        aHref8=''
+                        aText1='Careers'
+                        aText2='Blog'
+                        aText3='About Amazin'
+                        aText4='Investor Relations'
+                        aText5='Amazin Devices'
+                        aText6='Amazin Tours'
+                        aText7=''
+                        aText8=''
+                    />
+                    <FooterColumns
+                        footerTitle='Make Money with Us'
+                        aHref1='#'
+                        aHref2='#'
+                        aHref3='#'
+                        aHref4='#'
+                        aHref5='#'
+                        aHref6='#'
+                        aHref7='#'
+                        aHref8=''
+                        aText1='Sell products on Amazin'
+                        aText2='Sell apps on Amazin'
+                        aText3='Become an Affiliate'
+                        aText4='Advertise Your Products'
+                        aText5='Self-Publish with Us'
+                        aText6='Host an Amazin Hub'
+                        aText7='› See More Make Money with Us'
+                        aText8=''
+                    />
+                    <FooterColumns
+                        footerTitle='Amazin Payment Products'
+                        aHref1='#'
+                        aHref2='#'
+                        aHref3='#'
+                        aHref4='#'
+                        aHref5=''
+                        aHref6=''
+                        aHref7=''
+                        aHref8=''
+                        aText1='Amazin Business Card'
+                        aText2='Shop with Points'
+                        aText3='Reload Your Balance'
+                        aText4='Amazin Currency Converter'
+                        aText5=''
+                        aText6=''
+                        aText7=''
+                        aText8=''
+                    />
+                    <FooterColumns
+                        footerTitle='Let Us Help You'
+                        aHref1='#'
+                        aHref2='#'
+                        aHref3='#'
+                        aHref4='#'
+                        aHref5='#'
+                        aHref6='#'
+                        aHref7='#'
+                        aHref8='#'
+                        aText1='Amazin and COVID-19'
+                        aText2='Your Account'
+                        aText3='Your Orders'
+                        aText4='Shipping Rates & Policies'
+                        aText5='Returns & Replacements'
+                        aText6='Manage Your Content and Devices'
+                        aText7='Amazin Assistant'
+                        aText8='Help'
+                    />
                 </div>
 
                 <div className='footer__navFooterEmptySpaceLine'></div>
--- src/FooterColumns.js
+++ src/FooterColumns.js
@@ -1,18 +1,54 @@
 import React from 'react';
 import './FooterColumns.css';
 
-function FooterColumns() {
+function FooterColumns({
+    footerTitle,
+    aHref1,
+    aHref2,
+    aHref3,
+    aHref4,
+    aHref5,
+    aHref6,
+    aHref7,
+    aHref8,
+    aText1,
+    aText2,
+    aText3,
+    aText4,
+    aText5,
+    aText6,
+    aText7,
+    aText8,
+}) {
     return (
         <div className='footerColumns'>
             <div className='footer__navFooterVerticalColumn'>
-                <div className='footer__header'>Get to Know Us</div>
+                <div className='footer__header'>{footerTitle}</div>
                 <ul>
-                    <li>Careers</li>
-                    <li>Blog</li>
-                    <li>About Amazin</li>
-                    <li>Investor Relations</li>
-                    <li>Amazin Devices</li>
-                    <li>Amazin Tours</li>
+                    <li>
+                        <a href={aHref1}>{aText1}</a>
+                    </li>
+                    <li>
+                        <a href={aHref2}>{aText2}</a>
+                    </li>
+                    <li>
+                        <a href={aHref3}>{aText3}</a>
+                    </li>
+                    <li>
+                        <a href={aHref4}>{aText4}</a>
+                    </li>
+                    <li>
+                        <a href={aHref5}>{aText5}</a>
+                    </li>
+                    <li>
+                        <a href={aHref6}>{aText6}</a>
+                    </li>
+                    <li>
+                        <a href={aHref7}>{aText7}</a>
+                    </li>
+                    <li>
+                        <a href={aHref8}>{aText8}</a>
+                    </li>
                 </ul>
             </div>
         </div>
